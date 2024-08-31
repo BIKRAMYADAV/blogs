41935deda2c71f30624394a1774ae2fcecd5a7d6
bikram11git <bikram@myparticipants.com>
2024-09-01 00:40:37 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className='w-full flex justify-end p-3 pr-16 border-b-2'>
+        <a href="" className='mr-4'>Blogs</a>
+        <a href="" className=''>About</a>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
